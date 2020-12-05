af0e4ffdc3282dab9b5e16b00b2329d12e201189
Matt <Matt@VOLDEMORT>
2020-12-05 14:37:54 -0600

    Added homework 5

---
+++ cs2413/cs2413_hw5.cpp
@@ -0,0 +1,40 @@
+//HW 5
+
+
+class node {
+
+}
+
+class BST {
+public:
+
+
+	int insert_key(int key) {
+		if
+	}
+
+	int delete_key(int key) {
+
+	}
+
+	void enumerate() {
+
+	}
+
+private:
+
+	node* root = nullptr;
+
+}
+
+int main() {
+	BST tree;
+	int key;
+	int operation;	//Key defining which operation we will carry out on the tree
+	int operand;	//Key associated with the operator
+
+	//Input keys and build tree
+	while (std::cin >> key) {
+
+	}
+}
